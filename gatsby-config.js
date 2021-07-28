20661cdd906de985a62f5207c0312f9c3f7b3f47
MisiekKisiek <skrzypkka@gmail.com>
2021-07-28 15:05:50 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "MojePortfolio",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
